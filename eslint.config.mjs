8fff02f4ac32b80235daccfc1dc87475058d8afe
Philipp Niederlag <philipp.niederlag@artcom.de>
2024-12-16 16:06:08 +0100

    Update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2021,
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+]
